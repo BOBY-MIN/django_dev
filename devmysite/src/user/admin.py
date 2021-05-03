999e2ed37ff2667ddaf04ae0f49dfddc50308280
bobypoby123@gmail.com <@alsgmlalsdl16>
2021-05-03 14:55:12 +0900

    어드민 게시판 추가

---
+++ devmysite/src/user/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from user.models import DevUserBsc
+from user.models import DevBoardBsc
+# Register your models here.
+admin.site.register(DevUserBsc)
+admin.site.register(DevBoardBsc)
